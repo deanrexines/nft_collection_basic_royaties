7c01c91c6a9a85787f7558079f324d5e1ab55a6b
Dean Rexines <deanrex17@gmail.com>
2024-01-06 04:36:56 -0500

    Create BasicRoyalties.sol

---
+++ BasicRoyalties.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/common/ERC2981.sol";
+
+
+abstract contract BasicRoyalties is ERC2981 {
+
+    event DefaultRoyaltySet(address indexed receiver, uint96 feeNumerator);
+    event TokenRoyaltySet(uint256 indexed tokenId, address indexed receiver, uint96 feeNumerator);
+
+    constructor(address receiver, uint96 feeNumerator) {
+        _setDefaultRoyalty(receiver, feeNumerator);
+    }
+
+    function _setDefaultRoyalty(address receiver, uint96 feeNumerator) internal virtual override {
+        super._setDefaultRoyalty(receiver, feeNumerator);
+        emit DefaultRoyaltySet(receiver, feeNumerator);
+    }
+
+    function _setTokenRoyalty(uint256 tokenId, address receiver, uint96 feeNumerator) internal virtual override {
+        super._setTokenRoyalty(tokenId, receiver, feeNumerator);
+        emit TokenRoyaltySet(tokenId, receiver, feeNumerator);
+    }
+}
